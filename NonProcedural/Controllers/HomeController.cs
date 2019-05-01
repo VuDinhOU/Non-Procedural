f0c7ef91d50192320d37ceae7f0079387772830c
Vu Dinh <k1ngsley405@yahoo.com>
2019-05-01 16:23:03 -0500

    new store alert

--- NonProcedural/Controllers/HomeController.cs
+++ NonProcedural/Controllers/HomeController.cs
@@ -20,5 +20,52 @@ namespace NonProcedural.Controllers
             ViewBag.Title = "Create";
             return View();
         }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contact Page";
+            return View();
+                
+        }
+
+        public ActionResult Policy()
+        {
+            ViewBag.Title = "Policy Page";
+            return View();
+        }
+
+        public ActionResult Success()
+        {
+            ViewBag.Title = "Success Page";
+            return View();
+
+        }
+
+        public ActionResult Product()
+        {
+            ViewBag.Title = "Product Page";
+            return View();
+
+        }
+        public ActionResult Thanks()
+        {
+            ViewBag.Title = "Thanks Page";
+            return View();
+
+        }
+
+        public ActionResult Account()
+        {
+            ViewBag.Title = "Account Page";
+            return View();
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About Page";
+            return View();
+
+        }
     }
 }
