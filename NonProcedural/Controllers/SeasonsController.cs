ddeb7a48471b995eafa0bdc4a50b489ea95b1fff
Vu Dinh <k1ngsley405@yahoo.com>
2019-05-01 14:37:03 -0500

    layout

---
+++ NonProcedural/Controllers/SeasonsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NonProcedural.Controllers
+{
+    public class SeasonsController : Controller
+    {
+        // GET: Seasons
+        public ActionResult Seasons()
+        {
+            return View();
+        }
+    }
+}
