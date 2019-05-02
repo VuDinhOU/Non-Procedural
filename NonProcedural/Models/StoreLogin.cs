bb7e48d19848e3ca43912b512a841291a52ed4a4
Ryan Lim <Ryan Lim@MSI>
2019-05-01 19:42:48 -0500

    7

--- NonProcedural/Models/StoreLogin.cs
+++ NonProcedural/Models/StoreLogin.cs
@@ -11,11 +11,18 @@ namespace NonProcedural.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class StoreLogin
     {
         public int UserID { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        [DisplayName("User Name : ")]
         public string UserName { get; set; }
+        [DisplayName("Password :")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is required.")]
         public string Password { get; set; }
     }
 }
